19e740a83f475d98863c0e4910325fb3fd1621b7
RochollR <r.rocholl@wmdaten.com>
2024-11-05 22:34:57 +0100

    added spinner

--- app/src/main/java/de/fhdw/rroch/staticfix2/MainPage.java
+++ app/src/main/java/de/fhdw/rroch/staticfix2/MainPage.java
@@ -6,20 +6,23 @@ import android.widget.*;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.util.ArrayList;
+
 public class MainPage extends AppCompatActivity {
 
 
-    Button mBtnNavigateToFormsCollection;
-    Button mBtnNavigateToResults;
-    Button mBtnAddItem;
-    Button mBtnRandomItems;
-    Spinner mSpnInputParameters;
+
+    private ListView mListView;
+    private ArrayList<Integer> mItems;
+    private ArrayAdapter<Integer> mAdapter;
 
     @Override
     protected void onCreate(@Nullable @org.jetbrains.annotations.Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.main_layout);
+        Button mBtnNavigateToFormsCollection, mBtnNavigateToResults, mBtnAddItem,mBtnRandomItems;
+        Spinner mSpnInputParameters;
 
         mBtnNavigateToFormsCollection = findViewById(R.id.btn_form_page);
         mBtnNavigateToResults = findViewById(R.id.btn_result_page);
@@ -27,26 +30,43 @@ public class MainPage extends AppCompatActivity {
         mBtnRandomItems = findViewById(R.id.btn_random_items);
         mSpnInputParameters = findViewById(R.id.spn_input_parameters);
 
-        mBtnNavigateToFormsCollection.setOnClickListener(v -> {
-            Intent intent = new Intent(this, FormCollectionPage.class);
-            startActivity(intent);
-        });
+        mListView = findViewById(R.id.lv_main_page);
+        mItems = new ArrayList<>();
+        mAdapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, mItems);
+        mListView.setAdapter(mAdapter);
 
-        mBtnNavigateToResults.setOnClickListener(v -> {
-            Intent intent = new Intent(this, ResultPage.class);
-            startActivity(intent);
-        });
+        mSpnInputParameters.setAdapter(mAdapter);
 
-        //Spinner config
-        // Create an ArrayAdapter using the string array and a default spinner layout.
+        mBtnNavigateToFormsCollection.setOnClickListener(v -> navigateToFormsCollection());
+        mBtnNavigateToResults.setOnClickListener(v -> navigateToResults());
+
+
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(
                 this,
                 R.array.input_spinner_array,
                 android.R.layout.simple_spinner_item
         );
-        // Specify the layout to use when the list of choices appears.
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        // Apply the adapter to the spinner.
         mSpnInputParameters.setAdapter(adapter);
+
+        mBtnAddItem.setOnClickListener(v -> addItemListView());
+        mBtnRandomItems.setOnClickListener(v -> {});
+
+    }
+
+    private void navigateToFormsCollection() {
+        Intent intent = new Intent(this, FormCollectionPage.class);
+        startActivity(intent);
+    }
+    private void navigateToResults() {
+        Intent intent = new Intent(this, FormCollectionPage.class);
+        startActivity(intent);
+    }
+
+    private void addItemListView() {
+        int k = mItems.size();
+        mItems.add(k);
+        mAdapter.notifyDataSetChanged();
+        mListView.smoothScrollToPosition(mItems.size() - 1);
     }
 }
